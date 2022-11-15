00ad3f8bdab88892b97330cac6229aadfe3af925
ckwlghks123 <ckwlghks12+github2@gmail.com>
2022-11-15 23:34:47 +0900

    커피 메뉴 추가 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,44 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  const $menuName = $("#espresso-menu-name");
   const $menuForm = $("#espresso-menu-form");
 
   $menuForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    if ($menuName.value === "") {
+      alert("값을 입력해 주세요");
+      return;
+    }
+    const menuName = $("#espresso-menu-name").value;
+    const menuItemTemplate = (espressoMenuName) => `
+    <li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  </li>
+    `;
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate(menuName)
+    );
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").textContent = `총 ${menuCount}개`;
+    $menuName.value = "";
   });
-
-  const $menuName = $("#espresso-menu-name");
 
   $menuName.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
-      console.log($menuName.value);
     }
   });
 }
